d01c6db80f68888f83ab4aeb933acf5895977c88
Tazmondo <fondcoolman@outlook.com>
2020-12-23 01:39:19 +0000

    added cookies to client

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,8 @@ import 'package:shared_preferences/shared_preferences.dart' as prefs;
 import 'package:MobileApp/objects.dart';
 
 const String api = "http://10.0.2.2:5000";
-const String api2 = "http://195.188.1.165:5000"; // todo: Change to domain name later.
+const String api2 =
+    "http://195.188.1.165:5000"; // todo: Change to domain name later, or add option to set own ip.
 const String apiLogin = api + "/authenticate";
 const String apiTestAuth = api + "/testauth";
 const String apiPlaylist = api + "/playlist";
@@ -41,24 +42,21 @@ class PlaylistPage extends StatefulWidget {
 class _PlaylistPageState extends State<PlaylistPage> {
   final Set<Playlist> _playlists = Set();
   bool _loggedIn = false;
-  final http.Client _client = http.Client();
+  final MyClient _client = MyClient();
 
   void _loginCallback(bool isLoggedIn) {
     _loggedIn = isLoggedIn;
-    if (_loggedIn) Navigator.of(context).pop();
   }
 
   void _fetchPlaylists() {
-    setState(() {
-      if (!_loggedIn) {
-        Navigator.of(context)
-            .push(MaterialPageRoute(builder: (BuildContext context) {
-          return LoginRoute(_client, _loginCallback);
-        }));
-      } else {
-        // todo: fetch playlists if logged in
-      }
-    });
+    if (!_loggedIn) {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (BuildContext context) {
+        return LoginRoute(_client, _loginCallback);
+      }));
+    } else {
+      // todo: fetch playlists if logged in
+    }
   }
 
   @override
@@ -78,78 +76,116 @@ class _PlaylistPageState extends State<PlaylistPage> {
   }
 }
 
-class LoginRoute extends StatelessWidget {
-  final http.Client _client;
+class LoginRoute extends StatefulWidget {
+  final MyClient _client;
   final _loginCallback;
 
   LoginRoute(this._client, this._loginCallback);
 
   static final String _usernameDefault = "Username";
 
+  @override
+  _LoginRouteState createState() => _LoginRouteState();
+}
+
+class _LoginRouteState extends State<LoginRoute> {
+  bool _loggedIn = false;
+
   final TextEditingController _usernameController =
-      TextEditingController(text: _usernameDefault);
+      TextEditingController(text: LoginRoute._usernameDefault);
 
   final TextEditingController _passwordController = TextEditingController();
 
-  void _login() {
-    final String username = _usernameController.value.text;
-    final String password = _passwordController.value.text;
+  void _login(BuildContext context) {
+    setState(() {
+      final String username = _usernameController.value.text;
+      final String password = _passwordController.value.text;
 
-    final encoded = jsonEncode({'username': username, 'password': password});
-    print(encoded);
+      final encoded = jsonEncode({'username': username, 'password': password});
+      print(encoded);
 
-    _client.post(apiLogin, body: encoded, headers: {"Content-Type": "application/json"}).then((r) {
-      print("Login returned status code: ${r.statusCode}");
-      r.statusCode == 200 ? _loginCallback(true) : _loginCallback(false);
-    }, onError: (error) {
-      print(error);
+      widget._client.post(apiLogin,
+          body: encoded,
+          headers: {"Content-Type": "application/json"}).then((r) {
+        print("Login returned status code: ${r.statusCode}");
+        if (r.statusCode == 200) {
+          _loggedIn = true;
+        } else {
+          widget._client.get(apiTestAuth).then((r2) {
+            if (r2.statusCode == 200) {
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                      "New details incorrect, remaining logged in with old details."),
+                ),
+              );
+            } else {
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text("Details incorrect."),
+                ),
+              );
+              _loggedIn = false;
+            }
+          }, onError: (error) {
+            print(error);
+          });
+        }
+      }, onError: (error) {
+        print(error);
+      });
+      widget._loginCallback(_loggedIn);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Login to btecify"),
-        centerTitle: true,
-      ),
-      body: Container(
-        padding: EdgeInsets.all(16),
-        child: Center(
-          child: SizedBox(
-            width: 200,
-            height: 175,
-            child: Container(
-              color: Colors.black26,
-              padding: EdgeInsets.all(8),
-              child: Column(
-                children: [
-                  // Username entry
-                  TextField(
-                    autocorrect: false,
-                    controller: _usernameController,
-                    onTap: _usernameController.clear,
+    if (_loggedIn || true) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Login to btecify"),
+          centerTitle: true,
+        ),
+        body: Builder(builder: (BuildContext context) {
+          return Container(
+            padding: EdgeInsets.all(16),
+            child: Center(
+              child: SizedBox(
+                width: 200,
+                height: 175,
+                child: Container(
+                  color: Colors.black26,
+                  padding: EdgeInsets.all(8),
+                  child: Column(
+                    children: [
+                      // Username entry
+                      TextField(
+                        autocorrect: false,
+                        controller: _usernameController,
+                        onTap: _usernameController.clear,
+                      ),
+                      // Password entry
+                      TextField(
+                        autocorrect: false,
+                        controller: _passwordController,
+                        obscureText: true,
+                        onTap: _passwordController.clear,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          FocusScope.of(context).unfocus();
+                          _login(context);
+                        },
+                        child: Text("LOGIN"),
+                      ),
+                    ],
                   ),
-                  // Password entry
-                  TextField(
-                    autocorrect: false,
-                    controller: _passwordController,
-                    obscureText: true,
-                    onTap: _passwordController.clear,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      FocusScope.of(context).unfocus();
-                      _login();
-                    },
-                    child: Text("LOGIN"),
-                  ),
-                ],
+                ),
               ),
             ),
-          ),
-        ),
-      ),
-    );
+          );
+        }),
+      );
+    } else {}
   }
 }
--- lib/objects.dart
+++ lib/objects.dart
@@ -1,3 +1,5 @@
+import 'package:http/http.dart' as http;
+
 class Song {
   final String name;
   final String url;
@@ -12,8 +14,7 @@ class Song {
         duration = json['duration'],
         author = json['author'];
 
-  Map<String, dynamic> toJson() =>
-      {
+  Map<String, dynamic> toJson() => {
         'songname': name,
         'songurl': url,
         'duration': duration,
@@ -31,9 +32,29 @@ class Playlist {
       : name = json['playlistname'],
         songs = json['songs'];
 
-  Map<String, dynamic> toJson() =>
-      {
+  Map<String, dynamic> toJson() => {
         'songname': name,
         'songs': songs.map((e) => e.toJson()).toList(),
       };
+}
+
+class MyClient extends http.BaseClient {
+  http.Client _httpClient = http.Client();
+  String _cookies = "";
+
+  Future<http.StreamedResponse> send(http.BaseRequest request) async {
+    print(request);
+    if (!request.headers.containsKey('cookie')) {
+      request.headers['cookie'] = this._cookies;
+    }
+    http.StreamedResponse response = await _httpClient.send(request);
+    if (response.headers.containsKey('set-cookie')) {
+      this._cookies = response.headers['set-cookie'];
+    }
+    return response;
+  }
+}
+
+void main() {
+  MyClient().get('http://10.0.2.2:5000');
 }
